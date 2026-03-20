d30137449ea921b33a71515fc989919263b1038e
rahulsingh33304-pixel <rahulsingh33304@gmail.com>
2026-03-20 11:36:47 +0530

    Add files via upload

---
+++ coderahul.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+# import random
+
+
+# data = [random.randint(1, 20) for _ in range(500)]
+
+# plt.hist(data, bins=20)  # histogram
+# plt.title("Histogram Example")
+# plt.show()
+
+# used to find relationship btw variables
+
+#data'
+# y1 =[10,20,30,25,56]
+# y2=[15,25,35,45,35]
+
+# plt.scatter(y1,y2)
+# plt.title('scatter plot Example')
+# plt.show()
+
+# #data1, bar chart
+# categories = ['mon','tue','wed','thu','fri']
+# sales= [10,20,30,40,50,45,67]
+
+# #data 2 scetter chart
+# y1= [10,20,30,40,34]
+# y2=[20,30,35,45,55]
+# plt.figure(figsize=(10, 4))
+
+# # first plot -bar chart
+# plt.subplot(1,2,1) 
+# plt.bar(categories, sales)
+# plt.title("Daily Sales")
+# plt.xlabel("Week Days")
+# plt.ylabel("Sales")
+
+# #second plot scatter chart
+# plt.subplot(1,2,2)
+# plt.scatter(y1,y2)
+# plt.title("USe Example")
+# plt.xlabel("User1")
+# 
+
+
+# create of  
+import pandas as pd
+# data
+data={
+    'mont'
+}
